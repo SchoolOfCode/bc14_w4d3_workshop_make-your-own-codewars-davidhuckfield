029c36ecb27b0868f2f4119c82403707397c23fe
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-04-07 22:04:32 +0100

    Task 1 - wrote kata description and provided starting code

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 /* 
 👉 Write your kata here!
+
+Feed Sancho!
+
+Sancho the tortoise can only eat fruits & vegetables. He can't eat meat or dairy.
+
+You have to sort through an array of food objects and only return the foods that Sancho can eat
+
+Write a function that takes an array of objects and returns a new array with only the foods whose type is fruits or vegetables.
+
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+function feedSancho(foods) {
+    // 👉 Write your code here!
+}
