852eff732c83c52b0c7e9d8512992598b222e890
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-04-08 11:06:51 +0100

    Solved Jest issue with help from coach, first test now appears to be working correctly

--- main.js
+++ main.js
@@ -5,14 +5,55 @@ Feed Sancho!
 
 Sancho the tortoise can only eat fruits & vegetables. He can't eat meat or dairy.
 
-You have to sort through an array of food objects and only return the foods that Sancho can eat
+You have to sort through all the food objects in the fridge and only return the foods that Sancho can eat.
 
 Write a function that takes an array of objects and returns a new array with only the foods whose type is fruits or vegetables.
+
+You will only get input that is an array of food objects, examples below:
+
+E.g if input is [
+    {name: 'apple', type: 'fruit'}, 
+    {name: 'banana', type: 'fruit'}, 
+    {name: 'carrot', type: 'vegetable'}, 
+    {name: 'cheese', type: 'dairy'},
+    {name: 'chicken', type: 'meat'}
+] 
+then the output should be [
+    {name: 'apple', type: 'fruit'}, 
+    {name: 'banana', type: 'fruit'}, 
+    {name: 'carrot', type: 'vegetable'}, 
+]
 
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
 
-function feedSancho(foods) {
-    // 👉 Write your code here!
+// function feedSancho(foods) {
+//     // 👉 Write your code here!
+// }
+
+
+// potential answer - delete before final update!
+
+// let testData = [
+//     {name: 'apple', type: 'fruit'}, 
+//     {name: 'banana', type: 'fruit'}, 
+//     {name: 'carrot', type: 'vegetable'}, 
+//     {name: 'cheese', type: 'dairy'},
+//     {name: 'chicken', type: 'meat'}
+// ] 
+
+export function feedSancho(foods) {
+    // copilot suggested answer
+    // return foods.filter(food => food.type === 'fruit' || food.type === 'vegetable');
+    //what I would do: 
+    let sanchoFoods = [];
+    for (let i=0; i<foods.length; i++) {
+        if (foods[i].type === 'fruit' || foods[i].type === 'vegetable') {
+            sanchoFoods.push(foods[i]);
+        }
+    }
+    return sanchoFoods;
 }
+
+// console.log(feedSancho(testData));
--- main.test.js
+++ main.test.js
@@ -1,1 +1,31 @@
 //👉 Write your tests below here:
+
+import { test, expect } from '@jest/globals';
+import { feedSancho } from './main.js';
+
+test('Should return an array of objects whose type is either fruit or vegetable', function () {
+//ARRANGE
+//Set up test data
+
+let testData = [
+    {name: 'apple', type: 'fruit'}, 
+    {name: 'banana', type: 'fruit'}, 
+    {name: 'carrot', type: 'vegetable'}, 
+    {name: 'cheese', type: 'dairy'},
+    {name: 'chicken', type: 'meat'}
+] 
+//ACT
+//Set up expected response
+
+    let expected = [
+        {name: 'apple', type: 'fruit'}, 
+        {name: 'banana', type: 'fruit'}, 
+        {name: 'carrot', type: 'vegetable'}, 
+    ]
+
+
+//ASSERT
+//Check if both are equal
+
+    expect(feedSancho(testData)).toEqual(expected);
+  });
